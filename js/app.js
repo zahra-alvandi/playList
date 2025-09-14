9469a3c900495553a99f1e7e6eff9ab3cc42d774
Zahra Alvandi <zalvandi39@gmail.com>
2025-09-14 11:41:33 +0330

    volume and pause btn

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const playBtns = document.querySelectorAll(".play-btn");
 const music = document.querySelector("audio");
 const playIcon = document.querySelector(".play-icon");
 const playBtn = document.querySelector(".play-button");
+const volumeCard = document.querySelector(".volume-card");
+const volume = document.querySelector(".volume");
 
 playBtns.forEach(function (playBtn) {
     playBtn.addEventListener("click", function () {
@@ -13,6 +15,15 @@ playBtns.forEach(function (playBtn) {
             playIcon.classList.remove("fa-play");
             playIcon.classList.add("fa-pause");
         }
+
+        playBtns.forEach(function (playBtn) {
+            const playOrPauseIcon = playBtn.querySelector("i");
+            playOrPauseIcon.classList.remove("fa-pause")
+            playOrPauseIcon.classList.add("fa-pause");
+        })
+        const playOrPauseIcon = playBtn.querySelector("i");
+        playOrPauseIcon.classList.remove("fa-play");
+        playOrPauseIcon.classList.add("fa-pause");
     })
 })
 
@@ -26,4 +37,13 @@ playBtn.addEventListener("click", function () {
         playIcon.classList.add("fa-play");
         playIcon.classList.remove("fa-pause");
     }
+
+    const musicPauseIcon = document.querySelector(".play-btn .fa-pause");
+    musicPauseIcon.classList.remove("fa-pause");
+    musicPauseIcon.classList.add("fa-play");
+})
+
+volumeCard.addEventListener("click", function (event) {
+    music.volume = event.offsetX / 100;
+    volume.style.width = `${event.offsetX}px`;
 })
