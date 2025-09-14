83b3f8b08cf43be513a19f61b6c9b1581ec8de60
Zahra Alvandi <zalvandi39@gmail.com>
2025-09-14 12:00:56 +0330

    dynamic

--- js/app.js
+++ js/app.js
@@ -5,6 +5,15 @@ const playBtn = document.querySelector(".play-button");
 const volumeCard = document.querySelector(".volume-card");
 const volume = document.querySelector(".volume");
 
+const musics = [
+    { id: 1, title: "پژواک‌های نیمه‌شب ", cover: "./src/public/images/1.jpg", src: "./src/public/audios/1.mp3", singer: "لونا اسکای" },
+    { id: 2, title: "امواج فردا", cover: "./src/public/images/2.jpg", src: "./src/public/audios/2.mp3", singer: "افق نقره ای" },
+    { id: 3, title: "خاکسترهای فروزان", cover: "./src/public/images/3.jpg", src: "./src/public/audios/3.mp3", singer: "دره اسکارلت" },
+    { id: 4, title: "رویاهای نئونی", cover: "./src/public/images/4.jpg", src: "./src/public/audios/4.mp3", singer: "سایه الکتریکی" },
+    { id: 5, title: "زمزمه های باد", cover: "./src/public/images/5.png", src: "./src/public/audios/5.mp3", singer: "اورلیا نایت" },
+    { id: 6, title: "تعقیب غروب", cover: "./src/public/images/6.jpg", src: "./src/public/audios/6.mp3", singer: "خیال طلایی" },
+]
+
 playBtns.forEach(function (playBtn) {
     playBtn.addEventListener("click", function () {
         const mainMusicSrc = event.target.dataset.src;
